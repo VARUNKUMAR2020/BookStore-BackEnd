3981cc8df48f70ce71db99846861811993af5b18
VARUNKUMAR V <111338202+VARUNKUMAR2020@users.noreply.github.com>
2023-10-26 12:48:22 +0530

    Update dbConfig.js

--- Database/dbConfig.js
+++ Database/dbConfig.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
-const BASE_URL = process.env.NODE_ENVIRONMENT === 'development'
-                    ?   `mongodb://0.0.0.0:27017/${process.env.DEVELOPMENT_MONGO_DB_NAME}`
-                    : "mongodb+srv://root:root@cluster0.9ch90f3.mongodb.net/books" ;
+const BASE_URL ="mongodb+srv://root:root@cluster0.9ch90f3.mongodb.net/"
 
 mongoose.connect(BASE_URL)
 .then(response => {
